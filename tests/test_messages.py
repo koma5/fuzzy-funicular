cc8f037bfcc3e7beb431cf0a8bbf0144d4764f9a
PonyM <dev@lksch.ch>
2024-05-23 18:22:01 +0200

    improve generated model for discovery data, add test

--- tests/test_messages.py
+++ tests/test_messages.py
@@ -1,8 +1,14 @@
 import pytest
 
 from bt_mqtt_ui.models.device_telemetry import DeviceTelemetry
+from bt_mqtt_ui.models.tasmota_discovery import MQTTDiscovery
 
 
 @pytest.mark.parametrize("file", ("tele_tasmota_CEE97F_SENSOR.txt",))
 def test_telemetry_msg(file, test_data_message):
     model = DeviceTelemetry.model_validate(test_data_message(file))
+
+
+@pytest.mark.parametrize("file", ("tasmota_discovery_1.txt",))
+def test_discovery_msg(file, test_data_message):
+    model = MQTTDiscovery.model_validate(test_data_message(file))
